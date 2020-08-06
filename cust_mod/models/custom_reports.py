6ade3d9d451f9e3c41a13cee41fb46ccc59a6d98
ABXTTI <49796896+ABXTTI@users.noreply.github.com>
2020-08-06 19:44:09 +0500

    Add files via upload

--- cust_mod/models/custom_reports.py
+++ cust_mod/models/custom_reports.py
@@ -13,19 +13,36 @@ class FleetVehicle(models.Model):
 class PlannedWork (models.Model):
     _inherit = 'planned.work'
 
-    x_cost = fields.Float(string="Internal Cost")
-    x_com=fields.Float(string="Hourly Cost", compute="get_multiply")
-    # x_cost1 = fields.Float(string="Internal Cost", readonly=True)
-    x_com1 = fields.Float(string="Hourly Cost", compute="get_multiply")
-    @api.onchange('planned_work')
+    @api.depends('planned_work')
     def get_cost(self):
-        self.x_cost = self.planned_work.standard_price
+        for self in self:
+            self.x_cost = self.planned_work.standard_price
 
-    @api.multi
-    def get_multiply(self):
+    @api.depends('time_spent','duration')
+    def get_multiply2(self):
         for rec in self:
-            rec.x_com1=rec.duration*rec.x_cost
-            rec.x_com=rec.time_spent*rec.x_cost
+            rec.x_com = rec.time_spent * rec.x_cost
+            rec.x_com1 = rec.duration * rec.x_cost
+
+
+    x_cost = fields.Float(string="Internal Cost", compute="get_cost", store=True, readonly=True)
+    x_com = fields.Float(string="Total Cost", compute="get_multiply2", store=True, readonly=True)
+    x_com1 = fields.Float(string="Total Price", compute="get_multiply2", store=True, readonly=True)
+
+    # x_cost = fields.Float(string="Internal Cost")
+    # x_com=fields.Float(string="Hourly Cost", compute="get_multiply")
+    # # x_cost1 = fields.Float(string="Internal Cost", readonly=True)
+    # x_com1 = fields.Float(string="Hourly Cost", compute="get_multiply")
+    #
+    # @api.onchange('planned_work')
+    # def get_cost(self):
+    #     self.x_cost = self.planned_work.standard_price
+    #
+    # @api.multi
+    # def get_multiply(self):
+    #     for rec in self:
+    #         rec.x_com1=rec.duration*rec.x_cost
+    #         rec.x_com=rec.time_spent*rec.x_cost
 
     # @api.onchange('works_done')
     # def get_cost1(self):
@@ -45,20 +62,31 @@ class PlannedWork (models.Model):
 
 class MaterialUsed (models.Model):
     _inherit = 'material.used'
-    x_cost2 = fields.Float(string="Internal Cost", readonly=True)
-    x_com2 = fields.Float(string="Total Cost", compute="get_multiply2")
-    x_com3 = fields.Float(string="Total Price", compute="get_multiply2")
 
-    @api.onchange('material')
+    @api.depends('material')
     def get_cost2(self):
-        self.x_cost2 = self.material.standard_price
-        print self.x_cost2
+        for self in self:
+            self.x_cost2 = self.material.standard_price
 
-    @api.multi
+    @api.depends('amount')
     def get_multiply2(self):
-        for rec in self:
-            rec.x_com2= rec.amount * rec.x_cost2
-            rec.x_com3=rec.price*rec.amount
+         for rec in self:
+             rec.x_com2 = rec.amount * rec.x_cost2
+             rec.x_com3 = rec.price * rec.amount
+
+    x_cost2 = fields.Float(string="Internal Cost", compute="get_cost2", store=True, readonly=True)
+    x_com2 = fields.Float(string="Total Cost", compute="get_multiply2", store=True, readonly=True)
+    x_com3 = fields.Float(string="Total Price", compute="get_multiply2", store=True, readonly=True)
+
+    # @api.onchange('material')
+    # def get_cost2(self):
+    #     self.x_cost2 = self.material.standard_price
+
+    # @api.multi
+    # def get_multiply2(self):
+    #     for rec in self:
+    #         rec.x_com2 = rec.amount * rec.x_cost2
+    #         rec.x_com3 = rec.price * rec.amount
 
 # class CarWorkshop (models.Model):
 #     _name = 'car.workshop'
@@ -73,6 +101,9 @@ class MaterialUsed (models.Model):
 #     def get_multiply(self):
 #         for rec in self:
 #             rec.x_com1 = rec.duration * rec.x_cost
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+    job_ref = fields.Many2one('car.workshop','x_seq', string="Job Reference", required=True)
 
 
 
