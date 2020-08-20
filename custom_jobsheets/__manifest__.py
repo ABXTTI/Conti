4a2c070081eeb1be2ed1baffb09e64c439f73762
ABXTTI <49796896+ABXTTI@users.noreply.github.com>
2020-08-20 20:19:18 +0500

    Add files via upload

---
+++ custom_jobsheets/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Custom Jobs',
+    'version': '12.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'Module for custom reports in CarWorkshop',
+    'sequence': '201',
+    'license': 'AGPL-3',
+    'author': 'SB',
+    'maintainer': 'SB',
+    'website': 'sb.com',
+    'depends': ['base','purchase', 'fleet', 'fleet_car_workshop', 'account', 'account_accountant'],
+    'demo': [],
+    'data': [
+        'reports/job_sheet.xml'
+    ],
+    'images': ['static/description/banner.jpg','static/src/img/header.jpeg','static/src/img/footer.jpeg',],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+
+}
