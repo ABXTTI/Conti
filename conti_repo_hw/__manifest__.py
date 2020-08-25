5394f04c8d57d08674e46db513d30116cda0190f
ABXTTI <49796896+ABXTTI@users.noreply.github.com>
2020-08-26 00:46:04 +0500

    Add files via upload

--- conti_repo_hw/__manifest__.py
+++ conti_repo_hw/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Conti home world Reports',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'category': 'Extra Tools',
     'summary': 'Module for conti reports home world in sale order lines',
     'sequence': '202',
@@ -9,7 +9,7 @@
     'author': 'AB',
     'maintainer': 'AB',
     'website': 'ab.com',
-    'depends': ['base', 'account', 'account_accountant'],
+    'depends': ['account', 'account_accountant'],
     'demo': [],
     'data': [
         'views/views.xml',
--- conti_repo_hw/models/conti_repos_hw.py
+++ conti_repo_hw/models/conti_repos_hw.py
@@ -1,19 +1,49 @@
 from odoo import models, fields, api, _
-class ResCompany(models.Model):
-    _inherit = 'res.company'
-    round_invoice=fields.Boolean(string='Rounding Of Invoice')
+# class ResCompany(models.Model):
+#     _inherit = 'res.company'
+#     round_invoice=fields.Boolean(string='Rounding Of Invoice')
 
 class AccountInvoiceLine(models.Model):
     _inherit = 'account.invoice.line'
-    @api.one
-    @api.depends('price_unit')
-    def get_int(self):
-            if self.product_id.company_id.round_invoice:
-                self.price_subtotal=round(self.quantity*self.price_unit)
-            else:
-                self.price_subtotal = self.quantity * self.price_unit
+    x_discount_amount = fields.Float(string="Discount Amount")
 
-    price_subtotal=fields.Float(compute='get_int', store=True)
+    @api.one
+    @api.depends('price_unit', 'discount', 'x_discount_amount', 'invoice_line_tax_ids', 'quantity',
+                 'product_id', 'invoice_id.partner_id', 'invoice_id.currency_id', 'invoice_id.company_id',
+                 'invoice_id.date_invoice', 'invoice_id.date')
+    def _compute_price(self):
+        currency = self.invoice_id and self.invoice_id.currency_id or None
+        if self.x_discount_amount:
+            self.discount = self.x_discount_amount/self.price_unit * 100
+            price = self.price_unit - self.x_discount_amount
+        else:
+            if self.discount:
+                self.x_discount_amount = self.price_unit * self.discount/100
+            elif self.discount and self.x_discount_amount:
+                self.x_discount_amount = self.price_unit * self.discount/100
+            price = self.price_unit * (1 - (self.discount or 0.0) / 100.0)
+        taxes = False
+        if self.invoice_line_tax_ids:
+            taxes = self.invoice_line_tax_ids.compute_all(price, currency, self.quantity, product=self.product_id,
+                                                          partner=self.invoice_id.partner_id)
+        self.price_subtotal = price_subtotal_signed = taxes['total_excluded'] if taxes else self.quantity * price
+        if self.invoice_id.currency_id and self.invoice_id.company_id and self.invoice_id.currency_id != self.invoice_id.company_id.currency_id:
+            price_subtotal_signed = self.invoice_id.currency_id.with_context(
+                date=self.invoice_id._get_currency_rate_date()).compute(price_subtotal_signed,
+                                                                        self.invoice_id.company_id.currency_id)
+        sign = self.invoice_id.type in ['in_refund', 'out_refund'] and -1 or 1
+        self.price_subtotal_signed = price_subtotal_signed * sign
+
+
+    # @api.one
+    # @api.depends('price_unit')
+    # def get_int(self):
+    #         if self.product_id.company_id.round_invoice:
+    #             self.price_subtotal=round(self.quantity*self.price_unit)
+    #         else:
+    #             self.price_subtotal = self.quantity * self.price_unit
+    #
+    # price_subtotal=fields.Float(compute='get_int', store=True)
     # @api.one
     # @api.depends('invoice_line_ids.price_subtotal', 'tax_line_ids.amount', 'currency_id', 'company_id', 'date_invoice', 'type')
     # def _compute_amount(self):
